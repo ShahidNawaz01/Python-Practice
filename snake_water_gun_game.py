8c1d8dc00f2a9aed17a912f5f6a2fe386abc4f59
shahidnawaz <shahidnawaz.pieas@gmail.com>
2022-08-01 16:19:01 +0500

    added snake water gun game file

---
+++ snake_water_gun_game.py
@@ -0,0 +1,20 @@
+import random
+option = ['Snake', 'Water', 'Gun']
+random_var = random.choice(option)
+
+while True:
+    user_choice = input("Choose b/w 'Snake', 'Water' & 'Gun'. ")
+    if user_choice.lower() == 'snake' and random_var.lower() == 'water':
+        print('You win.')
+    elif user_choice.lower() == 'water' and random_var.lower() == 'gun':
+        print('You win.')
+    elif user_choice.lower() == 'gun' and random_var.lower() == 'snake':
+        print('You win.')
+    else:
+        print('You lose.')
+    count = input('Want to try again? (Y/N) ')
+    if count.lower() == 'y':
+        pass
+    else:
+        print('Better luck next time.')
+        break
